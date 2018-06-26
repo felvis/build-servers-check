0f5f23d33d9075b60bb11d485f01be3d6be00b45
Oleg Zavorin <felvisua@gmail.com>
2018-06-26 07:27:19 +0300

    Update github.sh

--- github.sh
+++ github.sh
@@ -17,5 +17,5 @@ PL_NUMBER=`echo $CODEBUILD_SOURCE_VERSION | cut -c 4-`
 if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 0 ]; then 
  curl -X POST "https://api.github.com/repos/felvis/build-servers-check/pulls/$PL_NUMBER/reviews" -H "authorization: Bearer $TOKEN" -H "content-type: application/json" -d '{"body":"Need to be fixed"}'; 
  else
- curl -X POST "https://api.github.com/repos/felvis/build-servers-check/pulls/$PL_NUMBER/reviews" -H "authorization: Bearer $TOKEN" -H "content-type: application/json" -d '{"body":"APPROVED. Link to artifacts - https://s3.amazonaws.com/com.shadowrobot.eu-open/'$BUILD_ID'/artifacts.zip"}'; 
+ curl -X POST "https://api.github.com/repos/felvis/build-servers-check/pulls/$PL_NUMBER/reviews" -H "authorization: Bearer $TOKEN" -H "content-type: application/json" -d '{"body":"APPROVED. Link to artifacts - https://s3.amazonaws.com/com.shadowrobot.eu-open/'$BUILD_ID'/"}'; 
  fi
